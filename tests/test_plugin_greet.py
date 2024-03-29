4044917ba38a91ff3ef6f05e32ca775956a589c1
nickole <nc427@njit.edu>
2024-03-29 13:51:20 -0400

    changes made to pytests

--- tests/test_plugin_greet.py
+++ tests/test_plugin_greet.py
@@ -1,5 +1,7 @@
 import pytest
 from app import App
+from app.calculator.calculator import Calculator
+
 
 def test_app_greet_command(capfd, monkeypatch):
     """Test that the REPL correctly handles the 'greet' command and outputs 'Hello, World!'."""
